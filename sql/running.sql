92351269714fed857e7b3d3ac0ae19a651235519
Dingxx <632932025@qq.com>
2015-09-06 23:06:32 +0800

    running.sql为最新的数据库sql语句

--- sql/running.sql
+++ sql/running.sql
@@ -1,17 +1,17 @@
--- °üº¬ÓÃ»§ĞÅÏ¢µÄ´´½¨
+ï»¿drop table userinformation;
+-- åŒ…å«ç”¨æˆ·ä¿¡æ¯çš„åˆ›å»º
 create table userinformation(
-	uid serial primary key,
+    uid serial primary key,
     umail varchar(30) not null,
     uname varchar(30) not null,
     upassword varchar(40),
     verifycode varchar(10),
     hasverify boolean,
     canalterpassword boolean
-    );
--- ÓÃ»§ºÃÓÑ±í
-insert into userinformation(umail,uname,upassword)
-values('c','c1','123456'),('d','d2','123456');
-select * from userinformation
+);
+
+
+-- ç”¨æˆ·å¥½å‹è¡¨
 create table userfriend(
 	id serial primary key,
 	uid int,
@@ -19,11 +19,8 @@ create table userfriend(
 	foreign key (uid) references userinformation (uid),
 	foreign key (fid) references userinformation (uid)
 );
-insert into userfriend(uid,fid)
-values(1,2),(2,1);
-select * from userfriend
-select a.uname from userinformation a,userfriend b where b.uid=1 and b.fid=a.uid
--- Â·ÏßĞÅÏ¢±í
+
+-- è·¯çº¿ä¿¡æ¯è¡¨
 create table routeinformation(
 	rid serial primary key,
 	createrid int,
@@ -33,28 +30,24 @@ create table routeinformation(
 	createtime timestamp default now() ,
 	foreign key (createrid) references userinformation (uid)
 );
-select ST_AsText(frompoint),ST_AsText(endpoint),ST_AsText(geom) from routeinformation where rid=1
-insert into routeinformation (createrid,frompoint,endpoint,geom)
-values(1,ST_GeomFromText('POINT(52 24)',4326),ST_GeomFromText('POINT(53 23)',4326),ST_GeomFromText('MultiLineString((52 24,52.34 23.12,52.65 23.45,53 23))',4326))
--- Ô¼ÅÜĞÅÏ¢±í
+
+-- çº¦è·‘ä¿¡æ¯è¡¨
 create table appointmentinfo(
 	aid serial primary key,
 	createrid int,
 	routeid int,
 	appointtime timestamp,
-	appointfriend text, /*[Êı×é]*/
+	appointfriend text, /*[æ•°ç»„]*/
 	foreign key (createrid) references userinformation (uid),
 	foreign key(routeid) references routeinformation(rid)
 );
-select aid from appointmentinfo order by aid desc limit 1
--- ±»Ô¼ÅÜµÄÓÃ»§±í
+
+-- è¢«çº¦è·‘çš„ç”¨æˆ·è¡¨
 create table appointfriendState(
 	afid serial primary key,
 	friendid int not null,
 	appid int not null,
-	state int default 0, /*0Î´¶Á£¬1Í¬Òâ£¬2¾Ü¾ø*/
+	state int default 0, /*0æœªè¯»ï¼Œ1åŒæ„ï¼Œ2æ‹’ç»*/
 	foreign key (friendid) references userinformation (uid),
 	foreign key(appid) references appointmentinfo(aid)
 );
-
-
