eae86be98d708be9e47cf5a2f0f8fa038d35f074
li <lihansunbai@163.com>
2015-09-08 14:18:28 +0800

    add diy-route model in home.heml and modified main.js with diy-route click func

--- myjs/main.js
+++ myjs/main.js
@@ -1,52 +1,77 @@
-(function($){
-	define(['myjs/LoginDialog','myjs/RegistDialog','myjs/mapcontrol','myjs/Route'],
-		function(Login,Regist,MapControl,Route) {
-		var initEvent=function(){
-			//关于我们
-			$("#aboutus").click(function (){
-			    if (!sideToggle.checked)
-			    {
-			        $("#sideToggle").trigger("click");
-			    }
-			    var me = this;
-			    me.html =
-			            '<div id="content">' +
-			            '<div id="information">' +
-			            '   <h4>刘文凯</h4>' +
-			            '   <h4>罗&nbsp;&nbsp;&nbsp;靓</h4>' +
-			            '   <h4>李晗孙白</h4>' + 
-			            '   <h4>张&nbsp;&nbsp;&nbsp;锐</h4>' +
-			            '</div>' + '</div>';
-			    //$(me.html).appendTo($('#sidemenu'));
-			    $('#content').replaceWith(me.html);
-			});
+(function ($) {
+    define(['myjs/LoginDialog', 'myjs/RegistDialog', 'myjs/mapcontrol', 'myjs/Route'],
+            function (Login, Regist, MapControl, Route) {
+                var initEvent = function () {
+                    //关于我们
+                    $("#aboutus").click(function () {
+                        if (!sideToggle.checked)
+                        {
+                            $("#sideToggle").trigger("click");
+                        }
+                        var me = this;
+                        me.html =
+                                '<div id="content">' +
+                                '<div id="information">' +
+                                '   <h4>罗&nbsp;&nbsp;&nbsp;靓</h4>' +
+                                '   <h4>李晗孙白</h4>' +
+                                '   <h4>丁雪兴</h4>' +
+                                '</div>' + '</div>';
+                        //$(me.html).appendTo($('#sidemenu'));
+                        $('#content').replaceWith(me.html);
+                    });
 
-			//登录对话框显示
-			var login=new Login();
-			$("#login").click(function(){
-				login.show();
-			});
+                    //自定义路线侧边栏
+                    $("#diy-road").click(function () {
+                        if (!sideToggle.checked)
+                        {
+                            $("#sideToggle").trigger("click");
+                        }
+                        var me = this;
+                        me.html =
+                                '<div id="makeroute">' +
+                                '<h3>自定义路线</h3>' +
+                                '   <h4>使用方法：</h4>' +
+                                '   <h4>1、在地图中使用点击右键选择设定起点</h4>' +
+                                '   <h4>2、选择跑步模式</h4>' +
+                                '   <h4>3、环圈跑和往返跑不需要设定终点。其他请自定义终点</h4>' +
+                                '   <h4>4、点击规划路线按钮，完成路线设计</h4>' +
+                                '<h3>输入跑步路程</h3>' +
+                                '<h4></h4>' +
+                                '</div>';
+                        //$(me.html).appendTo($('#sidemenu'));
+                        $('#content').replaceWith(me.html);
+                    });
 
-			//注册对话框显示
-			var register= new Regist();
-			$("#register").click(function(){
-				register.show();
-			});
+                    //登录对话框显示
+                    //var login = new Login();
+                    $("#login").click(function () {
+                        var login = new Login();
+                        login.show();
+                    });
 
-			//地图显示
-			var map=new MapControl({
-				div:'mapcontainer'
-			});
-			map.showZoomCon({
-				offset: [10, 130] //缩放平移控件的偏移值
-			});
+                    //注册对话框显示
+//                    var register = new Regist();
+                    $("#register").click(function () {
+                        var register = new Regist();
+                        register.show();
+                    });
 
-			//路线制定
-			var route=new Route({
-				map:map.map
-			});
-			route.rightclick();
-		};
-		initEvent();
-	});
+
+
+                    //地图显示
+                    var map = new MapControl({
+                        div: 'mapcontainer'
+                    });
+                    map.showZoomCon({
+                        offset: [10, 130] //缩放平移控件的偏移值
+                    });
+
+                    //路线制定
+                    var route = new Route({
+                        map: map.map
+                    });
+                    route.rightclick();
+                };
+                initEvent();
+            });
 })(jQuery);
